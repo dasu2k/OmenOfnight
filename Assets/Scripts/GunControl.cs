f2da8aad63575eb32bda4e662d1c19a4a284b496
Tulasidhar <110711019+dasu2k@users.noreply.github.com>
2024-03-20 22:25:58 +0530

    new environment

--- Assets/Scripts/GunControl.cs
+++ Assets/Scripts/GunControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
-
+using TMPro;
 public class GunControl : MonoBehaviour
 {
     //animations and shit
@@ -10,61 +10,103 @@ public class GunControl : MonoBehaviour
     [SerializeField] private GameObject bulletImpact;
     [SerializeField] private float delayBetweenBullets;
     [SerializeField] Animator gunAnimator;
+    [SerializeField] private GameObject playerView;
 
-
-
+    private SpriteRenderer gunSpriteRenderer = null;
+    public Sprite[] muzzleflash;
+    private bool canShoot;
+    //end
 
     
 
     //weapon active info
     [SerializeField] private float damage;
     [SerializeField] private int TotalAmmo;
+    private int ammoInMag;
+    [SerializeField] private int magSize;
+    //end
 
-
-    private SpriteRenderer gunSpriteRenderer = null;
-    public Sprite[] muzzleflash;
-    private bool canShoot;
     
+
+    //User interface shit
+    [SerializeField] private TMP_Text ammo;
+    //end
+    
+
+    //mechanics
     [SerializeField] private GameObject shootRayFrom; 
+    
+    //end 
+
     void Start()
     {
         damage = 1f;
         gunSpriteRenderer = GetComponentInChildren<SpriteRenderer>();
         canShoot = true;
-        TotalAmmo = 100;
+        ammoInMag = magSize;
+
     }
 
     void Update()
     {
-        if(Input.GetMouseButton(0) && canShoot){
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            reload();
+        }
+        if(Input.GetMouseButton(0) && canShoot && (TotalAmmo+ammoInMag > 0)){
             //shoot a bullet
+            if(ammoInMag <= 0)
+            {
+                reload();
+                ammo.text = ammoInMag + "/" + TotalAmmo; 
+            }
             shoot();
             //muzzle flash
             changeSprite(gunSpriteRenderer, muzzleflash[Random.Range(0,5)]);
             canShoot = false;
         }
+        else{
+            Debug.Log("no ammo left");
+        }
+
+        Debug.Log("Ammo in mag" + ammoInMag);
     }
 
+    void reload(){
+        gunAnimator.SetBool("isReloading",true);
+        if(TotalAmmo >= magSize){
+            TotalAmmo-= magSize-ammoInMag;
+            ammoInMag = magSize;
+        }
+        else{
+            ammoInMag = TotalAmmo;
+            TotalAmmo = 0;
+        }
+        ammo.text = ammoInMag + "/" + TotalAmmo;
+        Invoke("reloadComplete" , 0.7f);
+    }
+    void reloadComplete(){
+        gunAnimator.SetBool("isReloading",false);
+    }
     void canShootNow(){
         canShoot = true;
         gunAnimator.SetBool("isShooting",false);
-        shootRayFrom.GetComponent<Animator>().SetBool("isShooting",false);
+        playerView.GetComponent<Animator>().SetBool("isShooting",false);
     }
 
     void shoot(){
         gunAnimator.SetBool("isShooting",true);
-        shootRayFrom.GetComponent<Animator>().SetBool("isShooting",true);
+        playerView.GetComponent<Animator>().SetBool("isShooting",true);
         RaycastHit hit;
-        if(Physics.Raycast(gunBarrel.transform.position , Camera.main.transform.forward , out hit ,Mathf.Infinity)){
-            
+        if(Physics.Raycast(shootRayFrom.transform.position , Camera.main.transform.forward , out hit ,Mathf.Infinity)){
             Debug.Log(hit.collider.tag);
             if(hit.collider.tag == "Enemy"){
                 hit.collider.gameObject.GetComponent<EnemyTest>().takeDamage(damage);
             }
             Instantiate(bulletImpact,hit.point, Quaternion.identity);
-
         };
-        
+        ammoInMag--;
+        ammo.text = ammoInMag + "/" + TotalAmmo;
         Invoke("canShootNow" , delayBetweenBullets);
         //GetComponent<AudioSource>().Play();
     }
