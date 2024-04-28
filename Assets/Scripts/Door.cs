7769bd63cf7113e5b116369246b79f97bc224e48
Tulasidhar <tulasidharmulakaluri@gmail.com>
2024-04-28 21:56:38 +0530

    zombies!
    a new zombie model with animations and code

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField]bool rotateInwards;
+    GameObject player;
+    float playerDistance;
     // Start is called before the first frame update
+    bool open;
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        open = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerDistance = Vector3.Distance(transform.position,player.transform.position);
+        if(playerDistance > 4)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                if(!open){
+                    transform.Rotate(new Vector3(0f,90f,0f));
+                }
+                else{
+                    transform.Rotate(new Vector3(0f,0f,0f));
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ZombieControl.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieControl : MonoBehaviour
+{
+    private float distanceFromPlayer;
+    private GameObject player;
+    Animator animator;
+    [SerializeField] private float spottingRange;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float runningSpeed;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        animator= GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        distanceFromPlayer = Vector3.Distance(player.transform.position,transform.position);
+        
+        if(player != null){
+            if(distanceFromPlayer < attackRange)
+            {
+                //attack them
+                Debug.Log("attacking ");
+                animator.SetBool("attacking",true);
+                animator.SetBool("idle",false);
+                animator.SetBool("running",false);
+            }
+
+            else if(distanceFromPlayer < spottingRange)
+            {
+                //run towards them
+                Debug.Log("running towards player");
+                transform.LookAt(player.transform.position);
+                transform.Translate(Vector3.forward * Time.deltaTime  * runningSpeed);
+
+
+
+                animator.SetBool("attacking",false);
+                animator.SetBool("idle",false);
+                animator.SetBool("running",true);
+            }
+            else{
+                //go back into idle
+                Debug.Log("going into idle");
+
+            
+                animator.SetBool("attacking",false);
+                animator.SetBool("idle",true);
+                animator.SetBool("running",false);
+            }
+        }
+    }
+}
